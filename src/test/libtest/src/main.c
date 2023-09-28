fedcfd62de4867a6bfbdd39cc98f8550e24e94b8
hongzhiijun <hong.zhijun@wuqi-tech.com>
2023-09-28 14:12:21 +0800

    fix: update comment

--- src/test/libtest/src/main.c
+++ src/test/libtest/src/main.c
@@ -2,10 +2,15 @@
 #include "calc.h"
 
 /*
+https://www.cnblogs.com/eezhijun/p/17734333.html
+
 -I 指定保护头文件路径
 -L 指定库链接路径
 -l 指定库名称
-gcc src/mian.c -I include/ -L lib/ -l calc -o ./bin/main
+静态库：
+gcc src/main.c -I include/ -L lib/ -l calc -o ./bin/main
+动态库：
+gcc src/main.c -I include/ -L lib/ -Wl,-rpath="/home/hongzhijun/prj/leetcode/src/test/libtest/lib" -lcalc -o ./bin/main
 */
 
 int main(void)
