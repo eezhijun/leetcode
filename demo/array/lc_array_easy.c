cb07ef5d607ede7337e40834ce80bcc33e85c5a4
hongzhiijun <hong.zhijun@wuqi-tech.com>
2023-10-12 17:31:24 +0800

    fix: array->findPoisonedDuration

--- demo/array/lc_array_easy.c
+++ demo/array/lc_array_easy.c
@@ -19,6 +19,81 @@
 #include "uthash.h"
 
 /* 双指针 哈希表 数学 计数 排序 */
+/* https://leetcode.cn/problems/teemo-attacking/ */
+/* 在《英雄联盟》的世界中，有一个叫 “提莫” 的英雄。他的攻击可以让敌方英雄艾希（编者注：寒冰射手）进入中毒状态。
+
+当提莫攻击艾希，艾希的中毒状态正好持续 duration 秒。
+
+正式地讲，提莫在 t 发起攻击意味着艾希在时间区间 [t, t + duration - 1]（含 t 和 t + duration - 1）处于中毒状态。
+如果提莫在中毒影响结束 前 再次攻击，中毒状态计时器将会 重置 ，在新的攻击之后，中毒影响将会在 duration 秒后结束。
+
+给你一个 非递减 的整数数组 timeSeries ，其中 timeSeries[i] 表示提莫在 timeSeries[i] 秒时对艾希发起攻击，
+以及一个表示中毒持续时间的整数 duration 。
+
+返回艾希处于中毒状态的 总 秒数。
+
+
+示例 1：
+
+输入：timeSeries = [1,4], duration = 2
+输出：4
+解释：提莫攻击对艾希的影响如下：
+- 第 1 秒，提莫攻击艾希并使其立即中毒。中毒状态会维持 2 秒，即第 1 秒和第 2 秒。
+- 第 4 秒，提莫再次攻击艾希，艾希中毒状态又持续 2 秒，即第 4 秒和第 5 秒。
+艾希在第 1、2、4、5 秒处于中毒状态，所以总中毒秒数是 4 。
+示例 2：
+
+输入：timeSeries = [1,2], duration = 2
+输出：3
+解释：提莫攻击对艾希的影响如下：
+- 第 1 秒，提莫攻击艾希并使其立即中毒。中毒状态会维持 2 秒，即第 1 秒和第 2 秒。
+- 第 2 秒，提莫再次攻击艾希，并重置中毒计时器，艾希中毒状态需要持续 2 秒，即第 2 秒和第 3 秒。
+艾希在第 1、2、3 秒处于中毒状态，所以总中毒秒数是 3 。
+
+
+提示：
+
+1 <= timeSeries.length <= 104
+0 <= timeSeries[i], duration <= 107
+timeSeries 按 非递减 顺序排列 */
+/*
+    d=2 1 4 2+2=4
+    d=2 1 2 2+2-1=3
+    d=3 1 2 3+(2-1)
+*/
+int findPoisonedDuration(int* timeSeries, int timeSeriesSize, int duration)
+{
+    int low = 0, high = INT_MIN;
+    int ans = 0;
+    int i = 0;
+
+    for (i = 0; i < timeSeriesSize; i++) {
+        low = timeSeries[i];
+        if (low > high) {
+            high = low + duration - 1;
+            ans += duration;
+        } else if (low == high) {
+            high = low + duration - 1;
+            ans += duration - 1;
+        } else {
+            high = low + duration - 1;
+            ans += timeSeries[i] - timeSeries[i - 1];
+        }
+    }
+    return ans;
+}
+
+void findPoisonedDurationTest(void)
+{
+    int timeSeries[] = {1,3,5,7,9,11,13,15};
+    int timeSeriesSize = ARRAY_SIZE(timeSeries);
+    int duration = 4;
+    printf("input:duration=%d\n", duration);
+    PRINT_ARRAY(timeSeries, timeSeriesSize, "%d ");
+    printf("\n");
+    int ret = findPoisonedDuration(timeSeries, timeSeriesSize, duration);
+    printf("output:%d\n", ret);
+}
 
 /* https://leetcode.cn/problems/third-maximum-number/ */
 /* 给你一个非空数组，返回此数组中 第三大的数 。如果不存在，则返回数组中最大的数。
@@ -1631,5 +1706,6 @@ void lc_array_easy_test(void)
     // findDisappearedNumbersTest();
     // findContentChildrenTest();
     // islandPerimeterTest();
-    thirdMaxTest();
+    // thirdMaxTest();
+    findPoisonedDurationTest();
 }
