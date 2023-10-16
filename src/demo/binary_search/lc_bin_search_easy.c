f45366d1bca51db79dd453dfce91d7c99f29da77
hongzhijun <eehongzhijun@outlook.com>
2023-10-16 23:07:06 +0800

    feat: bin_search->mySqrt

--- src/demo/binary_search/lc_bin_search_easy.c
+++ src/demo/binary_search/lc_bin_search_easy.c
@@ -5,13 +5,67 @@
  * @version 0.1
  * @date 2023-10-16
  *
- * @copyright Copyright (c) 2023
+ * @copyr Copyr (c) 2023
  *
  */
 
 #include <stdio.h>
 
 #include "utils.h"
+
+/* https://leetcode.cn/leetbook/read/binary-search/xe9cog/ */
+/* 给你一个非负整数 x ，计算并返回 x 的 算术平方根 。
+
+由于返回类型是整数，结果只保留 整数部分 ，小数部分将被 舍去 。
+
+注意：不允许使用任何内置指数函数和算符，例如 pow(x, 0.5) 或者 x ** 0.5 。
+
+示例 1：
+
+输入：x = 4
+输出：2
+示例 2：
+
+输入：x = 8
+输出：2
+解释：8 的算术平方根是 2.82842..., 由于返回类型是整数，小数部分将被舍去。
+ 
+
+提示：
+
+0 <= x <= 231 - 1 */
+/*
+    x=8 mid=1+(8-1)/2=4
+    8/4=2 r=4-1=3 mid=1+(3-1)/2=2
+    l=2+1=3
+*/
+int mySqrt(int x)
+{
+    if (x < 2) {
+        return x;
+    }
+    int l = 1, r = x;
+    while (l <= r) {
+        int mid = l + (r - l) / 2;
+        if (x / mid == mid) {
+            return mid;
+        } else if (x / mid < mid) {
+            r = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+    return r;
+}
+
+int mySqrtTest(void)
+{
+    int x = 8;
+    printf("input:%d\n", x);
+    int ret = mySqrt(x);
+    printf("output:%d\n", ret);
+    return 0;
+}
 
 /* https://leetcode.cn/leetbook/read/binary-search/xexoac/ */
 /* 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target，
@@ -58,7 +112,7 @@ int search(int *nums, int numsSize, int target)
     return ret;
 }
 
-void searchTest(void)
+int searchTest(void)
 {
     int nums[] = {-1, 0, 3, 5, 9, 12};
     int numsSize = ARRAY_SIZE(nums);
@@ -68,6 +122,7 @@ void searchTest(void)
     PRINT_ARRAY(nums, numsSize, "%d ");
     int ret = search(nums, numsSize, target);
     printf("output:%d\n", ret);
+    return 0;
 }
 
 int lc_bin_search_easy_test(void)
