3009cf0de084ef0433e119b9b6cbbccd9b63cf4d
hongzhijun <eehongzhijun@outlook.com>
2023-10-18 22:23:10 +0800

    fix: bin_search->xsearch

--- src/demo/binary_search/lc_bin_search_easy.c
+++ src/demo/binary_search/lc_bin_search_easy.c
@@ -13,6 +13,34 @@
 
 #include "utils.h"
 
+/* https://leetcode.cn/leetbook/read/binary-search/xeog5j/ */
+int xsearch(int *nums, int numsSize, int target)
+{
+    int l = 0, r = numsSize - 1;
+    int ret = -1;
+    int mid;
+
+    while (l <= r) {
+        mid = l + (r - l) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[l] <= nums[mid]) {
+            if (nums[l] <= target && nums[mid] > target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        } else {
+            if (nums[r] >= target && nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+    }
+    return ret;
+}
+
 /* https://leetcode.cn/leetbook/read/binary-search/xee4ev/ */
 /**
  * Forward declaration of guess API.
