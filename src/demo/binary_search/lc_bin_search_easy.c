bc00680bd295ec525ef68e7d5801449a89973674
hongzhiijun <hong.zhijun@wuqi-tech.com>
2023-10-20 17:35:36 +0800

    fix: update bin_search
    1. add firstBadVersion
    2. add findPeakElement

--- src/demo/binary_search/lc_bin_search_easy.c
+++ src/demo/binary_search/lc_bin_search_easy.c
@@ -13,6 +13,122 @@
 
 #include "utils.h"
 
+/* 查找特定值 求算术平方根 */
+
+/* https://leetcode.cn/leetbook/read/binary-search/xem7js/ */
+/* 峰值元素是指其值严格大于左右相邻值的元素。
+
+给你一个整数数组 nums，找到峰值元素并返回其索引。数组可能包含多个峰值，在这种情况下，返回 任何一个峰值 所在位置即可。
+
+你可以假设 nums[-1] = nums[n] = -∞ 。
+
+你必须实现时间复杂度为 O(log n) 的算法来解决此问题。
+
+示例 1：
+
+输入：nums = [1,2,3,1]
+输出：2
+解释：3 是峰值元素，你的函数应该返回其索引 2。
+示例 2：
+
+输入：nums = [1,2,1,3,5,6,4]
+输出：1 或 5
+解释：你的函数可以返回索引 1，其峰值元素为 2；
+     或者返回索引 5， 其峰值元素为 6。
+
+提示：
+
+1 <= nums.length <= 1000
+-231 <= nums[i] <= 231 - 1
+对于所有有效的 i 都有 nums[i] != nums[i + 1] */
+int findPeakElement(int *nums, int numsSize)
+{
+    int l = 0, r = numsSize - 1;
+    int m = 0;
+
+    while (1) {
+        m = l + ((r - l) >> 1);
+        if (m + 1 <= r && nums[m] < nums[m + 1]) {
+            l = m + 1;
+        } else if (m - 1 >= l && nums[m] < nums[m - 1]) {
+            r = m;
+        } else {
+            break;
+        }
+    }
+    return m;
+}
+
+int findPeakElementTest(void)
+{
+    int nums[] = {2, 1}; // 3 2 1 1
+    int numsSize = ARRAY_SIZE(nums);
+
+    int ret = findPeakElement(nums, numsSize);
+    printf("output:%d\n", ret);
+    return 0;
+}
+
+/* https://leetcode.cn/leetbook/read/binary-search/xepthr/ */
+/* 你是产品经理，目前正在带领一个团队开发新的产品。不幸的是，你的产品的最新版本没有通过质量检测。由于每个版本都是基于之前的版本开发的，
+所以错误的版本之后的所有版本都是错的。
+
+假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
+
+你可以通过调用 bool isBadVersion(version) 接口来判断版本号 version 是否在单元测试中出错。实现一个函数来查找第一个错误的版本。
+你应该尽量减少对调用 API 的次数。
+
+示例 1：
+
+输入：n = 5, bad = 4
+输出：4
+解释：
+调用 isBadVersion(3) -> false
+调用 isBadVersion(5) -> true
+调用 isBadVersion(4) -> true
+所以，4 是第一个错误的版本。
+示例 2：
+
+输入：n = 1, bad = 1
+输出：1
+
+提示：
+
+1 <= bad <= n <= 231 - 1 */
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+bool isBadVersion(int version)
+{
+    int bad = 7;
+    return (version >= bad) ? true : false;
+}
+
+/* 1 2 3 4 5 6 7 */
+int firstBadVersion(int n)
+{
+    int l = 1, r = n;
+    int m;
+
+    while (l != r) {
+        m = l + ((r - l) >> 1);
+        if (isBadVersion(m)) {
+            r = m;
+        } else {
+            l = m + 1;
+        }
+    }
+    return l;
+}
+
+int firstBadVersionTest(void)
+{
+    int n = 7;
+    int ret = firstBadVersion(n);
+    printf("output:%d\n", ret);
+    return 0;
+}
+
 /* https://leetcode.cn/leetbook/read/binary-search/xeog5j/ */
 int xsearch(int *nums, int numsSize, int target)
 {
@@ -192,6 +308,9 @@ int searchTest(void)
 
 int lc_bin_search_easy_test(void)
 {
+    int ret = -1;
     // searchTest();
-    return 0;
+    // ret = firstBadVersionTest();
+    ret = findPeakElementTest();
+    return ret;
 }
