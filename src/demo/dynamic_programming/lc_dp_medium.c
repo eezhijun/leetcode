de384e2a1b831accdec5ae16babbf2d1d3191f7a
hongzhiijun <hong.zhijun@wuqi-tech.com>
2023-10-27 10:43:22 +0800

    fix: dp->canJump

--- src/demo/dynamic_programming/lc_dp_medium.c
+++ src/demo/dynamic_programming/lc_dp_medium.c
@@ -11,6 +11,20 @@
 
 #include "stdio.h"
 
+bool canJump(int *nums, int numsSize)
+{
+    int i;
+    int k = 0;
+
+    for (i = 0; i < numsSize; i++) {
+        if (i > k) {
+            return false;
+        }
+        k = (nums[i] + i) > k ? (nums[i] + i) : k;
+    }
+    return true;
+}
+
 /* https://leetcode.cn/problems/unique-paths/ */
 /*
 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
