9b0db0c7dfbe9847396d1f076e1a66e78576bdbc
hongzhiijun <hong.zhijun@wuqi-tech.com>
2023-11-01 14:30:57 +0800

    feat: add lc_bit_medium.c

--- src/demo/string/lc_string_easy.c
+++ src/demo/string/lc_string_easy.c
@@ -21,6 +21,19 @@
 
 /* 双指针 哈希表 栈 贪心 库函数 */
 
+/* https://leetcode.cn/problems/sparse-array-search-lcci/ */
+int findString(char **words, int wordsSize, char *s)
+{
+    int i;
+
+    for (i = 0; i < wordsSize; i++) {
+        if (strcmp(words[i], s) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 /* https://leetcode.cn/problems/unique-morse-code-words/ */
 /* 国际摩尔斯密码定义一种标准编码方式，将每个字母对应于一个由一系列点和短线组成的字符串， 比如:
 
